cb19df731df1953c652d6c2658c9367d4eb4ed17
Janice Alecha <janiceralecha@gmail.com>
2022-05-19 22:39:47 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+let a = 2000
+let b = 100
+
+const divide = function() {
+    return a / b
+}
+divide();
+
+let x = 2
+const square = (x) => x * 2;
+
+square();
+
+let c = 3
+let d = 4
+const add = (c, d) => ( c + d);
+
+add(c,d);
+
